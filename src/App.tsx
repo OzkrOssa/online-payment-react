ba3ae295373ae827858bfb696f6467108ad3e6bb
Oscar Ossa <oskrlex17@gmail.com>
2024-05-16 09:09:07 -0500

    feat: add tabs component with document and invoice forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Laptop from "@/assets/images/laptop.png";
+import { TabsComponent } from "@/components/tabs/tabs-component";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <div className="font-semibold text-3xl md:text-6xl lg:text-6xl xl:text-8xl xl:text-left text-center p-10">
           ¡Paga tu factura Red Planet fácil y rápido!
         </div>
+        {/* Tabs */}
+        <TabsComponent />
       </div>
       <div className="hidden md:block absolute inset-0 items-center justify-center md:static md:w-1/2">
         <img src={Laptop} alt="laptop" width={700} height={500} />
